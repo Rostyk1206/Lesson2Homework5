93b59bae70e370b3734c6f28d99e92553cad5987
unknown <rostykvoznyak@gmail.com>
2020-10-27 23:40:08 +0200

    Lesson2Homework5

---
+++ Lesson2Homework5/Lesson2Homework5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lesson2Homework5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14;
+            int R = 3;
+            int h = 4;
+            double V = pi * (R * R) * h;
+            double S = 2 * pi * R * (R + h);
+            Console.WriteLine($"V={V}\t S={S}");
+        }
+    }
+}
